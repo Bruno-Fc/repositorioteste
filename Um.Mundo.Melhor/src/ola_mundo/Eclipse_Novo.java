80b0a8c39741c4092efd4fb3e777d1644f55fd62
Bruno <88469825+Bruno-Fc@users.noreply.github.com>
2021-08-28 03:30:36 -0300

    vou conseguir

--- Um.Mundo.Melhor/src/ola_mundo/Eclipse_Novo.java
+++ Um.Mundo.Melhor/src/ola_mundo/Eclipse_Novo.java
@@ -6,7 +6,52 @@ public class Eclipse_Novo {
 
 	public static void main(String[] args) {
 		Scanner entrada=new Scanner(System.in);
+		 //tentando algo
+			
+	        int op= 0;
+	        float j, c, i,n;
+	        
+	        
+			do {
+				
+				System.out.print("[1.Juros]  ");
+				System.out.print("[2.Capital]  ");
+				System.out.print("[3.Taxa]  ");
+				System.out.print("[4.Prazo]  ");
+				System.out.print("[5.Sair]\n");
+		
+				System.out.print("\nOpção = ");
+				op = entrada.nextInt();
+				
+				if(op == 1) { //j = c.i.n/100
+					System.out.println("\t\t\t Juros Simples - Juros\n");
+					System.out.println("Formula: J=c.i.n/100");
+					System.out.print("\nCapital: R$ ");
+					c= entrada.nextFloat();
+					System.out.print("Taxa: ");
+					i= entrada.nextFloat();
+					System.out.print("Periodo: ");
+					n= entrada.nextFloat();
+					
+					j= c*i*n/100;
+					
+					System.out.printf("Juros de: R$ %.2f ",j);
+					/*System.out.println(".......................................................................");
+					System.out.println(".Capital: R$ "+ c + "| Taxa: "+ i+"%"+ "| Periodo: "+ n + "| Total de Juros: "+ j+" .");
+					System.out.println(".......................................................................");
+					*/
+					System.out.println("\n\n");
+				}
+			}
+			while( op != 5);
+			entrada.close();
+			
+			}
+	}					
+					
+					
+					
+					
+					
 
-	}
 
-}

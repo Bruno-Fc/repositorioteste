c6a97292abb0763c61d8ce17e6da6e8bca3d2582
Bruno <88469825+Bruno-Fc@users.noreply.github.com>
2021-08-28 02:52:28 -0300

    ven

--- Um.Mundo.Melhor/src/ola_mundo/Eclipse_Novo.java
+++ Um.Mundo.Melhor/src/ola_mundo/Eclipse_Novo.java
@@ -1,9 +1,11 @@
 package ola_mundo;
 
+import java.util.Scanner;
+
 public class Eclipse_Novo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner entrada=new Scanner(System.in);
 
 	}
 
